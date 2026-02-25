d0524f8ad6c84ad11659a47835470043f47af140
Robinson Castaño <robinson.casta29@gmail.com>
2026-02-25 15:52:53 -0500

    initial commit

---
+++ src/main/java/com/Taller/CICD_demo/CicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Taller.CICD_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Taller/CICD_demo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Taller.CICD_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
